b9110eddd974e1ce7aa79466c3e6718fe7e400a7
Ayberk Gerey <ayberkgerey@gmail.com>
2020-07-09 21:44:33 +0300

    Add files via upload

---
+++ Mortgage/src/com/geray/Main.java
@@ -0,0 +1,32 @@
+package com.geray;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner Scan = new Scanner(System.in);
+
+        System.out.print("Principal : ");
+        int principal = Scan.nextInt();
+
+        System.out.print("Annual Interest Rate : ");
+        float annualInterest = Scan.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT /MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years) : ");
+        byte years = Scan.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest , numberOfPayments) -1 );
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage : " + mortgageFormatted);
+    }
+}
